696c5c3a684d6d9872596d719291b6cebd3ce460
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-07-19 11:02:47 +0545

    Day 2

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,30 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
-    // All Listing
-    public static function all(){
-        return [
-            [
-                'id'=>1,
-                'title'=>'Listing One',
-                'description'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas vitae in laboriosam. Ipsam ea amet unde molestias, odit labore odio assumenda illo tenetur porro harum adipisci voluptatem corrupti dolor.'
-            ],
-            [
-                'id'=>2,
-                'title'=>'Listing Two',
-                'description'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas vitae in laboriosam. Ipsam ea amet unde molestias, odit labore odio assumenda illo tenetur porro harum adipisci voluptatem corrupti dolor.'
-            ],
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    // Single Listing
-    public static function find($id){
-        $listings = self::all();
-        foreach($listings as $listing){
-            if($listing['id']==$id){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
