c61921e0c6240a8e83436443ea6b828db7a82876
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-07-25 11:00:03 +0545

    Day 7 user login completed 3:37:20

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,4 +42,24 @@ class UserController extends Controller
 
         return redirect('/')->with('message', 'You have been logged out!');
     }
+
+     // Show login form
+     public function login(){
+        return view('users.login');
+    }
+
+     // login User
+     public function authenticate(Request $request){
+        $formFields=$request->validate([
+            'email'=>['required', 'email'],
+            'password'=>'required'
+        ]);
+        
+        if(auth()->attempt($formFields)){
+            $request->session()->regenerate();
+            return redirect('/')->with('message' ,'You are now logged in'); 
+        }
+        return back()->withErrors(['email'=>'Invalid Credentials'])->onlyInput('email'); // we did this for security reasons 
+        // that is hacker would not know if email or even password exists for the account
+    }
 }
