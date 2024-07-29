3980405fd38fd6d622f7d7b8bda57e12ae09ef61
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-07-29 13:11:01 +0545

    user registration and get token completed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash; // bcrypt
 
 class AuthController extends Controller
 {
     public function register(Request $request){
-        
+        // validation
+        $fields=$request->validate([
+            'name'=>'required|string',
+            'email'=>'required|string|unique:users,email',
+            'password'=>'required|confirmed',
+        ]);
+
+        // user creation
+        $user=User::create([
+            'name'=>$fields['name'],
+            'email'=>$fields['email'],
+            'password'=> bcrypt($fields['password'])
+        ]);
+
+        // token creation
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        // creating and sending the response
+        $response=[
+            'user'=>$user,
+            'token'=>$token
+        ];
+
+        return response($response, 201);
+
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\Api\ListingController;
 
@@ -29,6 +30,7 @@ Route::get('/posts', function(){
     });
     
 //  Public routes
+Route::post('/register', [AuthController::class, 'register']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('products/search/{name}', [ProductController::class, 'search']);
