8fffa4d2a51ca0163f5c3e0155c4dbdce353019f
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-07-29 11:15:55 +0545

    Protected routes completed

--- routes/api.php
+++ routes/api.php
@@ -16,30 +16,38 @@ use App\Http\Controllers\Api\ListingController;
 |
 */
 
-//  Protected routes
-Route::group(['middleware'=>['auth:sanctum']], function () {
-    Route::get('products/search/{name}', [ProductController::class, 'search']);
-});
 
-//  Public routes
+
 // SIMPLE API CREATION
 
 Route::get('/posts', function(){
     return response()->json([
         'posts'=>[
             'title'=>"Post One"
-        ]
+            ]
         ]);
-});
+    });
+    
+//  Public routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('products/search/{name}', [ProductController::class, 'search']);
+Route::get('listings', [ListingController::class, 'index']);
 
+//  Protected routes
+Route::group(['middleware'=>['auth:sanctum']], function () {
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
 
 // Route::get('/products', [ProductController::class, 'index']);
 // Route::post('/products', [ProductController::class, 'store']);
 // Route::get('/products/{id}', [ProductController::class, 'show']);
 
 // INSTEAD OF DOING ABOVE,  WE CAN DO THIS FOR BASIC CRUD APP and everything works
-Route::resource('products', ProductController::class);
+// Route::resource('products', ProductController::class);
 
-Route::get('listings', [ListingController::class, 'index']);
+
 
 
