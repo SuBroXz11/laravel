5064ad5c283be7f32d3d8e56e631ee9c24ac862e
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-07-30 10:19:13 +0545

    auth controller updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
         $fields=$request->validate([
             'name'=>'required|string',
             'email'=>'required|string|unique:users,email',
-            'password'=>'required|confirmed',
+            'password'=>'required|string|confirmed',
         ]);
 
         // user creation
@@ -46,17 +46,19 @@ class AuthController extends Controller
     public function login(Request $request){
         // validation
         $fields=$request->validate([
-            'name'=>'required|string',
-            'email'=>'required|string|unique:users,email',
-            'password'=>'required|confirmed',
+            'email'=>'required|string',
+            'password'=>'required|string',
         ]);
 
-        // user creation
-        $user=User::create([
-            'name'=>$fields['name'],
-            'email'=>$fields['email'],
-            'password'=> bcrypt($fields['password'])
-        ]);
+        // Check email
+        $user= User::where('email', $fields['email'])->first();
+
+        // Check password
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message'=>'Bad credentials'
+            ], 401);
+        }
 
         // token creation
         $token = $user->createToken('myapptoken')->plainTextToken;
--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,7 @@ Route::get('/posts', function(){
     
 //  Public routes
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('products/search/{name}', [ProductController::class, 'search']);
