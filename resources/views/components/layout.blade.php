8093d8bc8bf1e9276a765b771333b212539fa9e1
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-07-31 16:12:24 +0545

    nothing to change

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,7 @@
             crossorigin="anonymous"
             referrerpolicy="no-referrer"
         />
+        {{-- <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css" /> --}}
         <script src="//unpkg.com/alpinejs" defer></script>
         <script src="https://cdn.tailwindcss.com"></script>
         <script>
