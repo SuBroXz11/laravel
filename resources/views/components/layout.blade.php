30c4a5f4945fca0b6b8882189e4488076cbac36d
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-07-31 16:55:27 +0545

    navbar and footer updated

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="light">
     <head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -26,63 +26,87 @@
                 },
             };
         </script>
-        <title>LaraGigs | Find Laravel Jobs & Projects</title>
+        <style>
+            @keyframes slide-up {
+                from {
+                    transform: translateY(100%);
+                }
+                to {
+                    transform: translateY(0);
+                }
+            }
+            
+            .animate-slide-up {
+                animation: slide-up 1s ease-out;
+            }
+            </style>
+        <title>Jobster | Find Jobs & Projects</title>
     </head>
     <body class="mb-48">
-        <nav class="flex justify-between items-center mb-4">
-            <a href="/"
-                ><img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo"
-            /></a>
-            <ul class="flex space-x-6 mr-6 text-lg">
+        <div class="navbar bg-base-100 shadow-lg">
+            <div class="flex-1">
+                <a href="/">
+                    <img class="w-24" src="{{ asset('images/logo.png') }}" alt="Logo" class="logo" />
+                </a>
+            </div>
+            <div class="flex-none">
                 @auth
-                <li>
-                    <span class="font-bold uppercase">
-                        Welcome {{ auth()->user()->name }}
+                    <span class="font-bold uppercase text-xl mr-4">
+                        Welcome, {{ auth()->user()->name }}
                     </span>
-                </li>
-                <li>
-                    <a href="/listings/manage" class="hover:text-laravel"
-                        ><i class="fa-solid fa-gear"></i>
-                        Manage Listings</a
-                    >
-                </li>
-                <li>
-                    <form action="/logout" class="inline" method="POST">
-                    @csrf
-                <button type="submit"><i class="fa-solid fa-door-closed"></i>Logout</button>
-                </form>
-                </li>
-                @else
-                <li>
-                    <a href="/register" class="hover:text-laravel"
-                        ><i class="fa-solid fa-user-plus"></i> Register</a
-                    >
-                </li>
-                <li>
-                    <a href="/login" class="hover:text-laravel"
-                        ><i class="fa-solid fa-arrow-right-to-bracket"></i>
-                        Login</a
-                    >
-                </li>
-                @endauth
-            </ul>
-        </nav>
+                    <ul class="menu menu-horizontal px-1">
+                        <li class="relative">
+                            <details class="group">
+                                <summary class="flex items-center cursor-pointer">
+                                    <i class="fa-solid fa-gear text-xl mr-1"></i> 
+                                    <span class="text-xl">Settings</span>
+                                </summary>
+                                <ul class="bg-base-100 rounded-t-none p-2 absolute right-0 mt-1 z-10 shadow-lg group-open:mt-0 group-open:transition-all group-open:duration-200">
+                                    <li>
+                                        <a href="/listings/manage" class="flex items-center hover:text-laravel p-2">
+                                            <i class="fa-solid fa-list-check mr-2"></i> 
+                                            Manage Listings
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <form action="/logout" class="inline hover:text-laravel p-2" method="POST">
+                                            @csrf
+                                            <button type="submit" class="flex items-center">
+                                                <i class="fa-solid fa-door-closed mr-2"></i> Logout
+                                            </button>
+                                        </form>
+                                    </li>
+                                </ul>
+                            </details>
+                        </li>
+                    @else
+                        <li class="mr-4 list-none">
+                            <a href="/register" class="hover:text-laravel text-xl flex items-center">
+                                <i class="fa-solid fa-user-plus mr-2"></i> Register
+                            </a>
+                        </li>
+                        <li class="list-none">
+                            <a href="/login" class="hover:text-laravel text-xl flex items-center">
+                                <i class="fa-solid fa-arrow-right-to-bracket mr-2"></i> Login
+                            </a>
+                        </li>
+                    @endauth
+                    </ul>
+            </div>
+        </div>
+        
+          
     {{-- View Output --}}
     <main>
     {{-- @yield('content') --}}
     {{ $slot }}
 </main>
-<footer
-            class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
-        >
-            <p class="ml-2">Copyright &copy; 2024, All Rights reserved</p>
-
-            <a
-                href="/listings/create"
-                class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
-                >Post Job</a
-            >
-        </footer>
+<footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center animate-slide-up">
+    <p class="ml-2">&copy; 2024, All Rights Reserved</p>
+    <a href="/listings/create" class="absolute top-1/2 right-10 bg-black text-white py-2 px-5 transform -translate-y-1/2 hover:bg-laravel transition-all duration-300">
+        Post Job
+    </a>
+</footer>
         <x-flash-message/>
     </body>
 </html>
